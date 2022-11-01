99189b62435acc68903e4f5a08894ec532674ca1
TehJiaShuang <tehjiashuang@gmail.com>
2022-11-01 15:44:11 +0800

    [FIX] change indent

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './Booking.css';
+import Layout from './Components/ClassComponents';
+import "bootstrap/dist/css/bootstrap.min.css"
+import 'font-awesome/css/font-awesome.min.css'
 
-function App() {
+function ChangeWord() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"/>
+  <Layout/>
+  </>
   );
 }
 
-export default App;
+export default ChangeWord;
